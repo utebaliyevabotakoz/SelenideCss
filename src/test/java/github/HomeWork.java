ec0da5e842624d93648945d0653cc2cb73e57962
butebaliyeva <utebaliyevabotakoz@gmail.com>
2023-01-31 02:10:36 +0600

    update build.gradle

---
+++ src/test/java/github/HomeWork.java
@@ -0,0 +1,23 @@
+package github;
+
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class HomeWork {
+
+    @Test
+    void shouldFindSelenideRepositoryAtTheTop() {
+        // открыть браузер на странице гитхаб
+        open("https://github.com/");
+        // ввести в поле поиска "selenide" и нажать на enter
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        // в списке найденных репозиториев кликнуть на первый
+        $$("ul.repo-list li").first().$("a").click();
+        // проверка: в шапке репозитория текст "selenide/selenide"
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+
+    }
+}
