11c2d6e692a44fb640e7fadf07dfc47d03ee17a7
butebaliyeva <utebaliyevabotakoz@gmail.com>
2023-01-31 02:18:13 +0600

    Homework done

--- src/test/java/github/HomeWork.java
+++ src/test/java/github/HomeWork.java
@@ -4,20 +4,19 @@ package github;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class HomeWork {
 
     @Test
-    void shouldFindSelenideRepositoryAtTheTop() {
+    void shouldFindWiki() {
         // открыть браузер на странице гитхаб
-        open("https://github.com/");
-        // ввести в поле поиска "selenide" и нажать на enter
-        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
-        // в списке найденных репозиториев кликнуть на первый
-        $$("ul.repo-list li").first().$("a").click();
-        // проверка: в шапке репозитория текст "selenide/selenide"
-        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".wiki-rightbar").$$("li").last().$("button").click();
+        $(".wiki-rightbar").$(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
 
     }
 }
